b8ec0fb7c9475158fca92b53616bd6be3edb3d58
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-01 12:27:58 +0900

    add example2

---
+++ example2/src/ofApp.cpp
@@ -0,0 +1,80 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ae_easing.load("test.json");
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    float t = std::fmodf(ofGetElapsedTimef(), 14.0f);
+    float y = ae_easing.get(t);
+
+    // NOTE:
+    // - you can also specify property name: `ae_easing.get("prop name", t)`
+    // - alternatively, specify index: `ae_easing.get(0, t)` ( `.get(t)` is short for `.get(0, t)` )
+
+    ofDrawBitmapString("t: " + ofToString(t, 2), 50, 50);
+
+    ofDrawRectangle(100, y - 600, 50, 50);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
