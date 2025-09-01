0cd0912e0aefb2e1b07ab63982f3cbe4ab99ca3b
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-01 16:24:12 +0900

    feat: multi-track example

---
+++ example3/src/ofApp.cpp
@@ -0,0 +1,94 @@
+#include "ofApp.h"
+
+size_t property_index1;
+size_t property_index2;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ae_easing.load("test3.json");
+    property_index1 = ae_easing.getPropertyIndex("Position", "A");
+    property_index2 = ae_easing.getPropertyIndex("Rotation", "B");
+    
+//    ofSetRectMode(OF_RECTMODE_CENTER);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    float t = std::fmodf(ofGetElapsedTimef(), 6.0f);
+    ofVec2f p = ae_easing.get<ofVec2f>(t, property_index1);
+    float deg = ae_easing.get<float>(t, property_index2);
+
+    // NOTE:
+    // - you can also write directly: `.get(t, property_name, layer_name, [parent_name])`
+
+    ofDrawBitmapString("t: " + ofToString(t, 2), 50, 50);
+
+    ofDrawRectangle(p.x - 400, p.y - 100, 50, 50);
+    
+    ofPushStyle();
+    ofSetColor(255, 0, 0);
+    ofTranslate(200, 50);
+    ofRotateDeg(deg);
+    ofDrawRectangle(0, 0, 200, 50);
+    ofPopStyle();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
